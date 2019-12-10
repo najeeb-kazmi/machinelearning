359c153234805a8db9d8a769537a21aacd7ca706
Najeeb Kazmi <najeeb.kazmi@gmail.com>
2019-12-10 13:37:08 -0800

    Expose BasePredictor for EnsembleTrainer to EntryPoints

---
+++ src/Microsoft.ML.Data/EntryPoints/IEnsembleBasePredictor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ML.Runtime;
+using Microsoft.ML.Trainers;
+
+namespace Microsoft.ML.EntryPoints
+{
+    using TScalarTrainer = ITrainerEstimator<ISingleFeaturePredictionTransformer<IPredictorProducing<float>>, IPredictorProducing<float>>;
+
+    [TlcModule.ComponentKind("EnsembleBinaryBasePredictor")]
+    [BestFriend]
+    internal interface IEnsembleBinaryBasePredictorFactory : IComponentFactory<TScalarTrainer>
+    {
+
+    }
+
+}
